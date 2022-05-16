c09b808955386a21ab29a12c5561db32e6c0b102
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-05-16 11:43:35 -0500

    inquirer prompts

--- app.js
+++ app.js
@@ -1,4 +1,73 @@
 const cTable = require('console.table');
-const mysql = require('mysql');
 const inquirer = require('inquirer');
+const connection = require('./connection.js');
 
+//create connection n throw error if error
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    work();
+})
+
+const work = () => {
+    inquirer.prompt([{
+            name: "choice",
+            type: "list",
+            message: "What?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a new department",
+                "Add a new role",
+                "Add a new employee",
+                "Update employee role",
+                "Exit"
+            ]
+        }])
+        .then(function (answer) {
+            switch (answer.choice) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a new department":
+                    addDepartment();
+                    break;
+                case "Add a new role":
+                    addRole();
+                    break;
+                case "Add a new employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
+
+
+const viewAllDepartments = () => {
+    connection.query('SELECT * FROM departments', function (err, res) { 
+        if (err) throw err;
+        console.table(res);
+        work();
+    })
+}
+
+const viewAllRoles = () => {
+    connection.query('SELECT * FROM roles', function (err, res) { 
+        if (err) throw err;
+        console.table(res);
+        work();
+    })
+}
--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ require ('dotenv').config();
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: process.env.username,
+    user: process.env.DB_USERNAME,
     password: process.env.db_pw,
     database: process.env.db_name,
 })
