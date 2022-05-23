2a18d6b0a9b6cd49b261ffe8172918b664fd14dd
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-05-22 22:41:04 -0500

    test

--- queries.sql
+++ queries.sql
@@ -20,19 +20,19 @@ VALUES
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Jane', 'Smith', 1, NULL),
+    ('Joan', 'Smith', 1, NULL),
     ('John', 'Doe', 2, 1),
-    ('John', 'Williams', 3, 1),
+    ('Jeff', 'Thomas', 3, 1),
     ('Sally', 'Johnson', 4, 1),
     ('Bob', 'Smith', 5, 2),
-    ('Sally', 'Smith', 6, 2),
+    ('Joe', 'Smith', 6, 2),
     ('Sally', 'Williams', 7, 2),
     ('Sally', 'Johnson', 8, 2),
     ('Jane', 'Doe', 9, 3),
     ('Jane', 'Williams', 10, 3),
-    ('Jane', 'Johnson', 11, 3),
+    ('William', 'Johnson', 11, 3),
     ('John', 'Smith', 12, 4),
     ('John', 'Williams', 13, 4),
-    ('John', 'Johnson', 14, 4),
-    ('Sally', 'Smith', 15, 5),
+    ('Craig', 'Johnson', 14, 4),
+    ('Sally', 'Smothers', 15, 5),
     ('Sally', 'Williams', 16, 5);
